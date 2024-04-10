d12b1507b65dc5efc6c99549e850f7f4183f61a8
H31M44\Student <Student>
2024-04-10 18:21:15 +0700

    1

---
+++ src/main/java/vn/edu/iuh/fit/Kt1004Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kt1004Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kt1004Application.class, args);
+    }
+
+}
